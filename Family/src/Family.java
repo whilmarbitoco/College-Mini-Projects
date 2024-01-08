157ca42995e9f0f51bdd9434b8301d5dba8dd2b9
Whilmar Bitoco <whlmrbitoco@gmail.com>
2024-01-08 23:36:48 +0800

    added family project

---
+++ Family/src/Family.java
@@ -0,0 +1,14 @@
+
+public class Family {
+
+	String name;
+	int age;
+	String gender;
+	
+	public Family(String name, int age, String gender) {
+		this.name = name;
+		this.age = age;
+		this.gender = gender;
+		
+	}
+}
